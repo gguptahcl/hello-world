44053214060888a2c2a39c9c23a243e4b8e11bf6
Gaurav Gupta <g.gupta@hcl.com>
2018-09-25 16:53:41 -0700

    updated revision

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -4,9 +4,16 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 
 @RestController
 public class HelloController {
+
+
+   @Autowired
+    private Environment env;    
 
     @RequestMapping("/")
     public String index() {
@@ -14,9 +21,37 @@ public class HelloController {
     }
 
 
-	@RequestMapping(value="/hello/zip/{zipcode}", method = RequestMethod.GET)
-	public String getWeatherForZip(@PathVariable("zipcode") String zipcode) {   
-	   return "Getting weather from weather service for Zip Code :" + zipcode;
-	}
+     @RequestMapping(value="/hello/zip/{zipcode}", method = RequestMethod.GET)
+	public String getWeatherForZip(@PathVariable("zipcode") String zipcode) 
+  {   
+	String url ="http://" + "weather-service:8080" + "/weather/zip/" + zipcode ;  	
+	 try {
+	   RestTemplate restTemplate = new RestTemplate();
+	   String result = restTemplate.getForObject(url, String.class);
+	   return "In Hello Service : Getting weather from weather service for Zip Code :" + result;
+	  }
+	catch (Exception e) {
+		return url ;
+	}// end catch		
+  }// end method 
 
-}
+
+     @RequestMapping(value="/hello/{zipcode}", method = RequestMethod.GET)
+	public String getWeatherByCallingElb(@PathVariable("zipcode") String zipcode) 
+  {   
+	
+	String elbUrl = env.getProperty("ELB_DNS");
+   	String weatherServicePort = env.getProperty("WEATHER_SERVICE_PORT");
+	String url = "http://" + elbUrl + ":" + weatherServicePort + "/weather/zip/" + zipcode ;
+	try {
+		RestTemplate restTemplate = new RestTemplate();
+	        String result = restTemplate.getForObject(url, String.class);
+		return result;
+	  }
+	catch (Exception e) {
+		return url ;
+	}// end catch		
+  }// end method 
+
+}// end class
+
